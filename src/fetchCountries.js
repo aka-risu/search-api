fd5720b9b20cc013ee38163b3f9833e6ae9a2d1a
aka-risu <kyr.anastasiia@gmail.com>
2021-01-02 16:21:24 +0200

    fixes search

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,16 @@
 function fetchCountries(searchQuery) {
     return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
         .then(response => {
-            if(!response.ok) return Promise.reject("No result! Please try again.")
+            if (!response.ok) return Promise.reject("No result! Please try again.")
             return response.json()
+        }).then((array) => {
+            const arrayFiltered = array.filter(el => {
+                if (el.name.toLowerCase().includes(searchQuery.toLowerCase())) {
+                    return el
+                }
+            })
+            return arrayFiltered
         })
 }
+
 export default fetchCountries
